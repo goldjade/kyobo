bc34af36a5476d67e4784d5cd32b315f207b09c5
hyun <hgh6128@gmail.com>
2022-12-21 17:08:47 +0900

    날짜 이상하게 나오는 거 수정

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import Books from "./pages/Books";
 import NotFound from "./pages/NotFound";
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Books />} />
-                    <Route path="/books" element={<Books />} />
-                    <Route path="/books/:keyword" element={<Books />} />
-                    <Route path="/books/detail/:id" element={<BookDetail />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/books/:keyword" element={<Books />} />
+          <Route path="/books/detail/:id" element={<BookDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/d.jsx
+++ src/pages/d.jsx
@@ -1,24 +1,24 @@
 const [books, setBooks] = useState([]);
 const [page, setPage] = useState(0);
 const fetchDate = async () => {
-    const params = {
-        page: page,
-    };
-    const resultBest = await instance.get(requests.fetchBest, { params });
-    setBooks(resultBest.data.list);
+  const params = {
+    page: page,
+  };
+  const resultBest = await instance.get(requests.fetchBest, { params });
+  setBooks(resultBest.data.list);
 };
 const pageNext = () => {
-    setPage(page + 1);
+  setPage(page + 1);
 };
 const pagePrev = () => {
-    setPage(page - 1);
+  setPage(page - 1);
 };
 const changePage = (_page) => {
-    if (page !== _page) {
-        setPage(_page);
-        console.log("페이지바뀌어라", page);
-    }
+  if (page !== _page) {
+    setPage(_page);
+    console.log("페이지바뀌어라", page);
+  }
 };
 useEffect(() => {
-    fetchDate();
+  fetchDate();
 }, [page]);
